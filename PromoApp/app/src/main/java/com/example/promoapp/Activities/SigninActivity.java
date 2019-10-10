a3d99b8bcfa307eedb2f05aac79a387b950431bb
MartinCJ08 <martin.carrascoj@gmail.com>
2019-10-10 01:43:29 -0600

    Log in creado

---
+++ PromoApp/app/src/main/java/com/example/promoapp/Activities/SigninActivity.java
@@ -0,0 +1,87 @@
+package com.example.promoapp.Activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.promoapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SigninActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    TextView input_email,input_password;
+    Intent inMain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+        mAuth = FirebaseAuth.getInstance();
+        input_email = findViewById(R.id.input_email);
+        input_password = findViewById(R.id.input_password);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    public void onClick(View v){
+        String sEmail = input_email.getText().toString();
+        String sPass = input_password.getText().toString();
+
+        if(sEmail.isEmpty() || sPass.isEmpty()){
+            Toast.makeText(getApplicationContext(),"Fill the field",Toast.LENGTH_SHORT).show();
+        }else{
+            signIn(sEmail,sPass);
+        }
+
+        final ProgressDialog progressDialog = new ProgressDialog(this,
+                R.style.Theme_AppCompat_Light_Dialog_Alert);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Authenticating...");
+        progressDialog.show();
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+
+    public void signIn(String email,String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            inMain = new Intent(SigninActivity.this, MainActivity.class);
+                            startActivity(inMain);
+//                            updateUI(user);
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+        if(currentUser != null){}
+    }
+}
