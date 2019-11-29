b7e0f06800ad009e3558d3c5f00d24fe4f613164
Jonathan Mata <jonathan.mata@plumanegra.com.mx>
2019-11-28 21:06:21 -0700

    k pedo k pedo

--- PromoApp/app/src/main/java/com/example/promoapp/Fragments/HomeFragment.java
+++ PromoApp/app/src/main/java/com/example/promoapp/Fragments/HomeFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.example.promoapp.Activities.MainActivity;
@@ -15,12 +16,19 @@ import com.example.promoapp.R;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
 
 import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
     private ListView listView;
     private ItemAdapter adpter;
+    CarouselView carouselView;
+    int[] sampleImages = {R.drawable.promo0, R.drawable.promo1, R.drawable.promo2, R.drawable.promo3};
 
     @Nullable
     @Override
@@ -30,11 +38,23 @@ public class HomeFragment extends Fragment {
         adpter = new ItemAdapter(view.getContext(),getArrayItems());
         listView.setAdapter(adpter);
 
+        carouselView = view.findViewById(R.id.carouselView);
+        carouselView.setPageCount(sampleImages.length);
+        carouselView.setImageListener(imageListener);
 
         return view;
     }
     private ArrayList<Item> getArrayItems(){
         ArrayList<Item> listItems = new ArrayList<>();
+        // Test
+        /*listItems.add(new Item(R.drawable.domino,"DOMINOS","Una pizza gratis todos los dias","Hasta agotar existencias"));
+        listItems.add(new Item(R.drawable.fox,"ARTICFOX","Todos los colores a mitad de precion","Hasta agotar existencias"));
+        listItems.add(new Item(R.drawable.caffenio,"CAFFENIO","Refill gratis en la compra de cualquier cafe","Hasta agotar existencias"));
+        listItems.add(new Item(R.drawable.xiaomi,"XIAOMI","Un corta unas gratis en la compra de cualquier otro producto de MI","Hasta agotar existencias"));
+        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference peopleReference = FirebaseDatabase.getInstance().getReference();
+        Object people = peopleReference.child("user_promo").orderByChild("user").equalTo(uid);*/
+        // End Test
         for(int i=0; i< MainActivity.list.size();i++){
             switch(MainActivity.list.get(i)){
                 case "dominos":
@@ -53,5 +73,12 @@ public class HomeFragment extends Fragment {
         }
         return listItems;
     }
+
+    ImageListener imageListener = new ImageListener() {
+        @Override
+        public void setImageForPosition(int position, ImageView imageView) {
+            imageView.setImageResource(sampleImages[position]);
+        }
+    };
 
 }
